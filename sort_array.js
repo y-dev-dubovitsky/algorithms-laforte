e6c517c8562b6dacf27a16b4b9cc96168b56bba5
y.dubovitsky <y.dev.dubovitsky@gmail.com>
2025-01-17 12:48:32 +0300

    Бинарный поиск и удаление исправил

--- sort_array.js
+++ sort_array.js
@@ -8,9 +8,6 @@ export class SortArray {
     let lowBorder = 0;
     let upBorder = this.size;
 
-    console.log(lowBorder)
-    console.log(upBorder)
-
     while (true) {
       if (lowBorder > upBorder) {
         return false;
@@ -28,49 +25,33 @@ export class SortArray {
   }
 
   add(el) {
-    let low = 0;
-    let up = this.size;
-
-    if(this.size === 0) {
-      this.sortArray[this.size] = el;
-      this.size++
-      return
-    }
-
-    while (true) {
-      // console.log("low = " + low)
-      // console.log("up = " + up)
-      if(low === up) {
-        this.size++;
-        for (let i = low; i < this.size; i++) {
-          this.sortArray[i + 1] = this.sortArray[i];
-        }
-        this.sortArray[low] = el;
+    let j;
+    for (j = 0; j < this.size; j++) {
+      if (this.sortArray[j] > el) {
         break;
-      }
-      if (low > up) {
-        this.sortArray[this.size] = el;
-        this.size++;
-        break;
-      }
-      let cur = Math.floor((low + up) / 2);
-      if (this.sortArray[cur] === el) {
-        this.size++;
-        for (let i = cur; i < this.size; i++) {
-          this.sortArray[i + 1] = this.sortArray[i];
-        }
-        this.sortArray[cur] = el;
-        break;
-      }
-      if (this.sortArray[cur] < el) {
-        low = cur + 1;
-      } else {
-        up = cur - 1;
       }
     }
+    for (let k = j; k < this.size; k++) {
+      this.sortArray[k + 1] = this.sortArray[k];
+    }
+    this.sortArray[j] = el;
+    this.size++;
+  }
+
+  delete(el) {
+    let j;
+    for(j = 0; j < this.size; j++) {
+      if(this.sortArray[j] === el) {
+        break;
+      }
+    }
+    for(let i = j; i < this.size; i++) {
+      this.sortArray[i] = this.sortArray[i + 1]
+    }
+    this.size--
   }
 
   show() {
-    console.log(this.sortArray)
+    console.log(this.sortArray);
   }
 }
